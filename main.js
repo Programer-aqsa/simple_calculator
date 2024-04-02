0a2f49358a49ecd20e3941050f05b53c8b232c77
Aqsa Shahzad <160316789+Programer-aqsa@users.noreply.github.com>
2024-04-02 12:21:57 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const Answer = await inquirer.prompt([
+    {
+        message: "plase enter your first number",
+        type: "number",
+        name: "firstNumber",
+    },
+    {
+        message: "plase enter your seacond number",
+        type: "number",
+        name: "secondNumber",
+    },
+    {
+        message: "choose the opraters to perfoam opration",
+        type: "list",
+        name: "opraters",
+        choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+    },
+]);
+if (Answer.opraters === "Addition") {
+    console.log(Answer.firstNumber + Answer.secondNumber);
+}
+else if (Answer.opraters === "Subtraction") {
+    console.log(Answer.firstNumber - Answer.secondNumber);
+}
+else if (Answer.opraters === "Multiplication") {
+    console.log(Answer.firstNumber * Answer.secondNumber);
+}
+else if (Answer.opraters === "Division") {
+    console.log(Answer.firstNumber / Answer.secondNumber);
+}
+else {
+    console.log("plase enter valid oprators");
+}
+;
